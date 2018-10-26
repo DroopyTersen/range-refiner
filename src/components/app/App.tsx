066558d328600f89d82462fd136eedc1b99c9095
Petersen <apetersen@skylinetechnologies.com>
2018-10-26 12:10:48 -0500

    got initial typescript parcel app workign

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default class App extends React.PureComponent<AppProps, {}> {
+    render() {
+        return (
+            <div>App2</div>
+        );
+    }
+}
+
+export interface AppProps {
+    //props
+}
